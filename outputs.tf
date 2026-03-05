c5f1838f5bb06f8ec6852293329d6c88a395b895
Pushkar Laulkar <pushkar.laulkar@yahoo.com>
2026-03-05 13:37:23 +0400

    Define outputs for EC2 module IPs and NLB DNS
    Added outputs for control plane, worker, and GPU node IPs, as well as NLB DNS name.

---
+++ outputs.tf
@@ -0,0 +1,22 @@
+# Accessing the control plane IPs from the ec2 module
+output "control_plane_ips" {
+  description = "Public and Private IPs for Control Plane nodes"
+  value       = module.oik8s_ec2.control_plane_ips
+}
+
+# Accessing the worker node IPs from the ec2 module
+output "worker_node_ips" {
+  description = "Public and Private IPs for Worker nodes"
+  value       = module.oik8s_ec2.worker_node_ips
+}
+
+# Accessing the GPU node IPs from the ec2 module
+output "gpu_node_ips" {
+  description = "Public and Private IPs for GPU nodes"
+  value       = module.oik8s_ec2.gpu_node_ips
+}
+
+output "nlb_dns_name" {
+  description = "API server endpoint"
+  value       = "${module.oik8s_nlb.control_plane_nlb_dns}:6443"
+}
